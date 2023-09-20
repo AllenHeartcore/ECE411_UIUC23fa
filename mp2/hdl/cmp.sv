6898cd2a7b1e070290e852a2689b9f9be4d1a65f
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-09-20 02:25:46 -0500

    Add CMP operations support (draft)

---
+++ mp2/hdl/cmp.sv
@@ -0,0 +1,20 @@
+
+module cmp
+import rv32i_types::*;
+(
+    input cmp_ops cmpop,
+    input [31:0] a, b,
+    output logic [31:0] f
+);
+
+always_comb
+begin
+    unique case (cmpop)
+		cmp_eq : f = (a == b);
+		cmp_ne : f = (a != b);
+		cmp_lt : f = (a < b);
+		cmp_ge : f = (a >= b);
+    endcase
+end
+
+endmodule : cmp
