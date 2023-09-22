f92fbb30d6bece6743659b8dbc62b96289663177
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-09-22 04:39:31 -0500

    MP1 Pt.4 (cacheline adaptor) done

--- mp1/cacheline_adaptor/hdl/cacheline_adaptor.sv
+++ mp1/cacheline_adaptor/hdl/cacheline_adaptor.sv
@@ -19,4 +19,68 @@ module cacheline_adaptor
     output logic write_o,
     input resp_i
 );
+
+
+logic [63:0] buffer [0:3];
+enum logic [3:0] {
+    IDLE, R1, R2, R3, R4, W1, W2, W3, W4, DONE
+} state, next_state;
+
+
+always_ff @(posedge clk or negedge reset_n) begin
+    if (!reset_n)
+        state <= IDLE;
+    else begin
+        state <= next_state;
+        if (resp_i)
+            {buffer[3], buffer[2], buffer[1], buffer[0]} <=
+            {  burst_i, buffer[3], buffer[2], buffer[1]};
+    end
+end
+
+
+always_comb begin
+
+    read_o = 1'b0;
+    write_o = 1'b0;
+    resp_o = 1'b0;
+    burst_o = 64'h0;
+    next_state = state;
+    address_o = address_i;
+    line_o = {buffer[3], buffer[2], buffer[1], buffer[0]};
+
+    unique case (state)
+
+        IDLE: begin
+            if (read_i) begin
+                next_state = R1;
+                read_o = 1'b1;
+            end else if (write_i) begin
+                next_state = W1;
+                write_o = 1'b1;
+            end
+        end
+
+        R1: begin read_o  = 1'b1; if (resp_i) next_state = R2;   end
+        R2: begin read_o  = 1'b1; if (resp_i) next_state = R3;   end
+        R3: begin read_o  = 1'b1; if (resp_i) next_state = R4;   end
+        R4: begin read_o  = 1'b1; if (resp_i) next_state = DONE; end
+
+        W1: begin write_o = 1'b1; if (resp_i) next_state = W2;   burst_o = line_i[ 63:  0]; end
+        W2: begin write_o = 1'b1; if (resp_i) next_state = W3;   burst_o = line_i[127: 64]; end
+        W3: begin write_o = 1'b1; if (resp_i) next_state = W4;   burst_o = line_i[191:128]; end
+        W4: begin write_o = 1'b1; if (resp_i) next_state = DONE; burst_o = line_i[255:192]; end
+
+        DONE: begin
+            next_state = IDLE;
+            resp_o = 1'b1;
+            read_o = 1'b0;
+            write_o = 1'b0;
+        end
+
+    endcase
+
+end
+
+
 endmodule : cacheline_adaptor
