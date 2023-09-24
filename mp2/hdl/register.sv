34d29491ce1ba305b0b1e30bdc3a3f04c1c5aff8
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-09-24 00:15:20 -0500

    [Style] Add "reset value" port in register module

--- mp2/hdl/register.sv
+++ mp2/hdl/register.sv
@@ -4,6 +4,7 @@ module register
     input clk,
     input rst,
     input load,
+    input [31:0] rst_value,
     input [31:0] in,
     output [31:0] out
 );
@@ -11,7 +12,7 @@ module register
 logic [31:0] data;
 always_ff @( posedge clk ) begin : reg_ff
     if (rst) begin
-        data <= '0;
+        data <= rst_value;
     end else if (load) begin
         data <= in;
     end
