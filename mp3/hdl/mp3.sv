41e7115dc0ad08fff54740b990ddfc91724e6348
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-09-29 22:54:59 -0500

    Connect CPU, cache, adapters at top level

--- mp3/hdl/mp3.sv
+++ mp3/hdl/mp3.sv
@@ -11,10 +11,45 @@ import rv32i_types::*;
     input   logic           bmem_resp
 );
 
+    // cpu <-> bus_adapter
+    logic [ 31:0] mem_address, mem_rdata, mem_wdata;
+    logic [  3:0] mem_byte_enable;
+    logic         mem_read, mem_write, mem_resp;
+
+    // bus_adapter <-> cache
+    logic [255:0] mem_rdata256, mem_wdata256;
+    logic [ 31:0] mem_byte_enable256;
+
+    // cache <-> cacheline_adaptor
+    logic [255:0] pmem_rdata, pmem_wdata;
+    logic [ 31:0] pmem_address;
+    logic         pmem_read, pmem_write, pmem_resp;
+
     cpu cpu(.*);
 
-    cache cache(.*);
+    bus_adapter bus_adapter(.*,
+        .address    (mem_address)
+    );
 
-    // Anything else?
+    cache cache(.*,
+        .mem_rdata  (mem_rdata256),
+        .mem_wdata  (mem_wdata256),
+        .mem_byte_enable(mem_byte_enable256)
+    );
+
+    cacheline_adaptor cacheline_adaptor(.*,
+        .address_i  (pmem_address),
+        .line_o     (pmem_rdata),
+        .line_i     (pmem_wdata),
+        .read_i     (pmem_read),
+        .write_i    (pmem_write),
+        .resp_o     (pmem_resp),
+        .address_o  (bmem_address),
+        .burst_i    (bmem_rdata),
+        .burst_o    (bmem_wdata),
+        .read_o     (bmem_read),
+        .write_o    (bmem_write),
+        .resp_i     (bmem_resp)
+    );
 
 endmodule : mp3
