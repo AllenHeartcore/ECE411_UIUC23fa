19f0984cbd7582c26cdde7b7c01aedd972d6fed7
Ziyuan Chen <ziyuanc3@illinois.edu>
2023-11-05 01:33:50 -0500

    [Style] Move signal definitions outside always_comb in fwd unit

--- mp4/hdl/cpu/forwarding.sv
+++ mp4/hdl/cpu/forwarding.sv
@@ -12,9 +12,10 @@ import pipeline_pkg::*;
     output  fwdmux::fwdmux_sel_t fwdmux1_sel, fwdmux2_sel
 );
 
+    logic fwd_from_mem_possible, fwd_from_wb_possible, fwd_to_1_possible, fwd_to_2_possible;
+
     always_comb begin
 
-        logic fwd_from_mem_possible, fwd_from_wb_possible, fwd_to_1_possible, fwd_to_2_possible;
         fwd_from_mem_possible = ctrlwb_at_mem.load_regfile & (ctrlwb_at_mem.rd != 5'b0);
         fwd_from_wb_possible  = ctrlwb_at_wb.load_regfile  & (ctrlwb_at_wb.rd != 5'b0);
         fwd_to_1_possible = (
