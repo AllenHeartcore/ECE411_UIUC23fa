a6762b49eba4b3a720aa76585729bf10f6f38a20
Hao Ren <moomoohorse738834256@gmail.com>
2023-11-08 21:36:09 -0600

    [Style] Change forwarding always_comb style, remove potential dependencies

--- mp4/hdl/cpu/forwarding.sv
+++ mp4/hdl/cpu/forwarding.sv
@@ -18,12 +18,10 @@ import pipeline_pkg::*;
     logic instr_at_mem_writes_rd, instr_at_wb_writes_rd;
     logic instr_at_ex_reads_rs1,  instr_at_ex_reads_rs2;
 
-    always_comb begin
-
-        instr_at_mem_reads_dmem = ctrlmem_at_mem.dmem_read   & (ctrlwb_at_mem.rd != 5'b0);
-        instr_at_mem_writes_rd  = ctrlwb_at_mem.load_regfile & (ctrlwb_at_mem.rd != 5'b0);
-        instr_at_wb_writes_rd   = ctrlwb_at_wb.load_regfile  & (ctrlwb_at_wb.rd  != 5'b0);
-        instr_at_ex_reads_rs1 = (
+    assign instr_at_mem_reads_dmem = ctrlmem_at_mem.dmem_read   & (ctrlwb_at_mem.rd != 5'b0);
+    assign instr_at_mem_writes_rd  = ctrlwb_at_mem.load_regfile & (ctrlwb_at_mem.rd != 5'b0);
+    assign instr_at_wb_writes_rd   = ctrlwb_at_wb.load_regfile  & (ctrlwb_at_wb.rd  != 5'b0);
+    assign instr_at_ex_reads_rs1 = (
             ctrlmem_at_ex.opcode == op_imm |
             ctrlmem_at_ex.opcode == op_reg |
             ctrlmem_at_ex.opcode == op_load |
@@ -31,19 +29,21 @@ import pipeline_pkg::*;
             ctrlmem_at_ex.opcode == op_jalr |
             ctrlmem_at_ex.opcode == op_br
         );
-        instr_at_ex_reads_rs2 = (
+    assign instr_at_ex_reads_rs2 = (
             ctrlmem_at_ex.opcode == op_reg |
             ctrlmem_at_ex.opcode == op_store |
             ctrlmem_at_ex.opcode == op_br
-        );
-
+    );
+    always_comb begin : FWDMUX_SEL1
         if ((~instr_at_mem_reads_dmem) & instr_at_mem_writes_rd & instr_at_ex_reads_rs1 & (ctrlwb_at_mem.rd == ctrlwb_at_ex.rs1))
             fwdmux1_sel = fwdmux::fwd_mem;
         else if(instr_at_wb_writes_rd & instr_at_ex_reads_rs1 & (ctrlwb_at_wb.rd  == ctrlwb_at_ex.rs1))
             fwdmux1_sel = fwdmux::fwd_wb;
         else
             fwdmux1_sel = fwdmux::no_fwd;
+    end
 
+    always_comb begin : FWDMUX_SEL2
         if ((~instr_at_mem_reads_dmem) & instr_at_mem_writes_rd & instr_at_ex_reads_rs2  & (ctrlwb_at_mem.rd == ctrlwb_at_ex.rs2))
             fwdmux2_sel = fwdmux::fwd_mem;
         else if(instr_at_wb_writes_rd & instr_at_ex_reads_rs2 & (ctrlwb_at_wb.rd  == ctrlwb_at_ex.rs2))
