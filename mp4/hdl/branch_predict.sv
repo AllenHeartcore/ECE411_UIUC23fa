a736055e2218b8d063adbadc7c5390caac1ed2c3
Zhirong Chen <zhirong.20@intl.zju.edu.cn>
2023-11-14 22:15:36 -0600

    finish branch predictor

---
+++ mp4/hdl/branch_predict.sv
@@ -0,0 +1,55 @@
+module branch_predictor 
+#(
+    parameter int N = 4 // Define N as per your requirement
+)
+(
+    input logic clk,
+    input logic reset,
+    input logic valid,
+    input logic actual_branch_taken,
+    output logic prediction
+);
+
+// Parameters for FSM states
+typedef enum logic [1:0] {
+    STRONG_TAKEN = 2'b11, 
+    WEAK_TAKEN = 2'b10, 
+    WEAK_NOT_TAKEN = 2'b01, 
+    STRONG_NOT_TAKEN = 2'b00
+} state_t;
+
+
+
+// Branch History Register
+logic [N-1:0] BHR; 
+
+// Pattern History Table
+state_t PHT[2**N-1:0]; 
+
+
+// Update BHR and PHT
+always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+        BHR <= '0;
+        prediction <= '0;
+        for (int i = 0; i < 2**N; i++) begin
+            PHT[i] <= WEAK_NOT_TAKEN;
+        end
+    end else begin
+        // Update the BHR
+        if (valid) begin
+            BHR <= {BHR[N-2:0], actual_branch_taken};
+        end
+        // Make a prediction based on the PHT
+        prediction <= (PHT[BHR] == STRONG_TAKEN || PHT[BHR] == WEAK_TAKEN);
+
+        // Update the PHT based on the actual outcome
+        if (actual_branch_taken) begin
+            if (PHT[BHR] != STRONG_TAKEN) PHT[BHR] <= PHT[BHR] + 1;
+        end else begin
+            if (PHT[BHR] != STRONG_NOT_TAKEN) PHT[BHR] <= PHT[BHR] - 1;
+        end
+    end
+end
+
+endmodule
---
+++ mp4/hvl/branch_predictor_tb.sv
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+module branch_predictor_tb;
+
+// Parameters
+localparam int N = 4;
+
+// Testbench signals
+logic clk;
+logic reset;
+logic valid;
+logic actual_branch_taken;
+logic prediction;
+
+// Instantiate the unit under test (UUT)
+branch_predictor #(.N(N)) uut (
+    .clk(clk),
+    .reset(reset),
+    .valid(valid),
+    .actual_branch_taken(actual_branch_taken),
+    .prediction(prediction)
+);
+
+// Clock generation
+always #5 clk = ~clk; // Generate a clock with a period of 10 ns
+
+// Test sequence
+initial begin
+    // Initialize testbench signals
+    clk = 0;
+    valid = 0;
+    actual_branch_taken = 0;
+
+    // Reset the UUT
+    #10 reset = 0;
+    #10 reset = 1;
+    #10 reset = 0;
+    #10
+
+    // Test a sequence of branches
+    repeat (2**N) begin
+        @(posedge clk); // Wait for the positive edge of the clock
+        valid = 1;
+        actual_branch_taken = $random; // Randomize the actual branch taken signal
+        @(posedge clk); // Wait for the UUT to update its prediction
+        valid = 0;
+
+        // Check the prediction
+        if (actual_branch_taken != prediction) begin
+            $display("Mismatch detected. Time: %t, Actual: %b, Prediction: %b", $time, actual_branch_taken, prediction);
+        end else begin
+            $display("Match detected. Time: %t, Actual: %b, Prediction: %b", $time, actual_branch_taken, prediction);
+        end
+    end
+
+    // Finish the simulation
+    #100;
+    $finish;
+end
+
+endmodule
